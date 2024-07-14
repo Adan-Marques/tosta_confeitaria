7604405fb583f2ca2b58db1b731e36d95438b301
Adan Marques <adan__marx@hotmail.com>
2024-07-13 23:54:05 -0300

    criacao classe produto

---
+++ api-back/src/main/java/br/com/projeto/api_back/model/Produto.java
@@ -0,0 +1,33 @@
+package br.com.projeto.api_back.model;
+
+public class Produto {
+    private long id;
+    private String nome;
+    private String descricao;
+    private float preco;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}

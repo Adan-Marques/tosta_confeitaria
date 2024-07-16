3a7b6800c4f5106fc77b8f05eda9251aea485b26
Adan Marques <adan__marx@hotmail.com>
2024-07-15 22:31:04 -0300

    mudancas visuais

--- api-front/src/app/app.routes.ts
+++ api-front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 export const routes: Routes = [
 
     {path:"home", component:HomeComponent},
     {path:"login", component:LoginComponent},
+    {path:"carrinho", component:CarrinhoComponent},
 ];

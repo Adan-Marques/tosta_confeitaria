5fb5276f7b8b5ca300bd94e1dc267df09daa236a
Adan Marques <adan__marx@hotmail.com>
2024-07-16 23:45:07 -0300

    desenvolvimento da aba de detalhes do produto

--- api-front/src/app/app.routes.ts
+++ api-front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 export const routes: Routes = [
 
     {path:"home", component:HomeComponent},
     {path:"login", component:LoginComponent},
     {path:"carrinho", component:CarrinhoComponent},
+    {path:"detalhes/:id", component: DetalhesComponent},
 ];
--- api-front/src/app/detalhes/detalhes.component.ts
+++ api-front/src/app/detalhes/detalhes.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../service/produto.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { Produto } from '../model/Produto';
 
 @Component({
   selector: 'app-detalhes',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent ,CommonModule],
   templateUrl: './detalhes.component.html',
   styleUrl: './detalhes.component.css'
 })
-export class DetalhesComponent {
+export class DetalhesComponent implements OnInit{
+
+  produto!: Produto;
+
+  constructor(private servico: ProdutoService, private route: ActivatedRoute) {
+
+  }
+
+    // Método de inicialização
+    ngOnInit(){
+      // this.route.paramMap.subscribe(() => {
+      //   this.selecionar_id();
+      // })
+    }
+
+  
+  // TODO (implementação com backend)
+  //     // Método de seleção
+  // selecionar_id(): void {
+  //   const id = Number(this.route.snapshot.paramMap.get('id'));
+  //   console.log(id);
+
+  //   this.servico.selecionar_id(id).subscribe(
+  //     (produto) => {
+  //       this.produto = produto; // atribui o produto recebido do serviço à variável local
+  //       console.log(this.produto); // confira se o produto está sendo recebido corretamente
+  //     },
+  //     (error) => {
+  //       console.error('Erro ao carregar produto:', error);
+  //     }
+  //   );
+  // }
 
 }

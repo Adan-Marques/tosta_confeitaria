84081f33b756ddc8dcd05410b3bcb8b0da341766
Adan Marques <adan__marx@hotmail.com>
2024-07-18 17:20:51 -0300

    carrinho

--- api-front/src/app/carrinho/carrinho.component.ts
+++ api-front/src/app/carrinho/carrinho.component.ts
@@ -9,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class CarrinhoComponent {
 
+  // public lista: Item[] = [];
+  // public mensagem: String = "";
+  // public totalCesta: number = 0;
+
+  // constructor(){
+  //   let json = localStorage.getItem("cesta");
+  //   if(json==null){
+  //     this.mensagem = "Sua cesta de compras esta vazia!";
+  //   } 
+  //   else {
+  //     this.lista = JSON.parse(json);
+  //     for(let item of this.lista){
+  //       this.totalCesta = this.totalCesta + item.total;
+  //     }
+  //   }
+  // }
+
+  // limpar(){
+  //   this.lista = [];
+  //   localStorage.removeItem("cesta");
+  // }
+
+  // redirectToVitrine(){
+  //   window.location.href="./vitrine";
+  // }
+
+  // formatarNumero(numero:number) {
+  //   if (typeof numero !== 'number') {
+  //       return ''; // trata valores não numéricos
+  //   }
+  //   // Formata o número para o padrão brasileiro
+  //   return numero.toFixed(2).replace('.', ',');
+  // }
+
 }

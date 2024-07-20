0e9588d31079a683bbfa98d258b346526ff5bfb3
Adan Marques <adan__marx@hotmail.com>
2024-07-19 23:42:49 -0300

    mudancas no carrinho

--- api-front/src/app/carrinho/carrinho.component.ts
+++ api-front/src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from '../model/Produto';
 
 @Component({
   selector: 'app-carrinho',
@@ -9,22 +10,22 @@ import { Component } from '@angular/core';
 })
 export class CarrinhoComponent {
 
-  // public lista: Item[] = [];
-  // public mensagem: String = "";
-  // public totalCesta: number = 0;
+  public lista: Produto[] = [];
+  public mensagem: String = "";
+  public totalCarrinho: number = 0;
 
-  // constructor(){
-  //   let json = localStorage.getItem("cesta");
-  //   if(json==null){
-  //     this.mensagem = "Sua cesta de compras esta vazia!";
-  //   } 
-  //   else {
-  //     this.lista = JSON.parse(json);
-  //     for(let item of this.lista){
-  //       this.totalCesta = this.totalCesta + item.total;
-  //     }
-  //   }
-  // }
+  constructor(){
+    let json = localStorage.getItem("carrinho");
+    if(json==null){
+      this.mensagem = "Sua cesta de compras esta vazia!";
+    } 
+    else {
+      this.lista = JSON.parse(json);
+      for(let item of this.lista){
+        this.totalCarrinho = this.totalCarrinho + item.preco;
+      }
+    }
+  }
 
   // limpar(){
   //   this.lista = [];
@@ -35,12 +36,12 @@ export class CarrinhoComponent {
   //   window.location.href="./vitrine";
   // }
 
-  // formatarNumero(numero:number) {
-  //   if (typeof numero !== 'number') {
-  //       return ''; // trata valores não numéricos
-  //   }
-  //   // Formata o número para o padrão brasileiro
-  //   return numero.toFixed(2).replace('.', ',');
-  // }
+  formatarNumero(numero:number) {
+    if (typeof numero !== 'number') {
+        return ''; // trata valores não numéricos
+    }
+    // Formata o número para o padrão brasileiro
+    return numero.toFixed(2).replace('.', ',');
+  }
 
 }
